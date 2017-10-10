dccdc49cb9978c73d4808db1657897172fa64e3c
Manokar <mganesamoorthy@thapovan-inc.com>
2017-10-10 18:41:42 +0530

    Add Buttons

---
+++ firestore_demo_web/app.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+	var btnAdd = document.querySelector("#btnAdd");
+	var btnFetch = document.querySelector("#btnFetch");
+
+	btnAdd.addEventListener('click',function(){
+
+	});
+
+	btnFetch.addEventListener('click', function(){
+
+	});
+	
+}
